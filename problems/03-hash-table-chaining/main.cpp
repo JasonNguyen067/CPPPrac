51ce218a8e8c05986c84ee87485aeed78204b50f
Your Name <jasonnguyen7506@gmail.com>
2026-08-24 00:30:00 -0700

    chaining for hash table

--- problems/03-hash-table-chaining/main.cpp
+++ problems/03-hash-table-chaining/main.cpp
@@ -1,4 +1,78 @@
+#include <iostream>
 
+struct Node {
+    int key;
+    Node* next;
+};
+
+class HashTable {
+private: 
+    Node* table[10];
+public: 
+    HashTable() {
+        for (int i = 0; i < 10; i++) {
+            table[i] = nullptr;
+        }
+    }
+
+    int hashFunction(int key) {
+        return key % 10;
+    }
+
+    void insert(int key) {
+        int index = hashFunction(key);
+
+        Node* newNode = new Node;
+        newNode->key = key;
+        newNode->next = table[index];
+        table[index] = newNode;
+    }
+
+    void display() {
+        for (int i = 0; i < 10; i++) {
+            Node* curr = table[i];
+            while (curr != nullptr) {
+                std::cout << curr->key;
+                curr = curr->next;
+            }
+        }
+    }
+
+    bool search(int key) {
+        int index = hashFunction(key);
+        for (int i = 0; i < 10; i++) {
+            Node* curr = table[index];
+
+            while (curr != nullptr) {
+                if (curr->key == key) {
+                    return true;
+                }
+                curr = curr->next;
+            }
+            return false;
+        }
+    }
+
+    bool remove(int key) {
+        int index = hashFunction(key);
+        Node* curr = table[index];
+        Node* prev = nullptr;
+
+        while (curr != nullptr) {
+            if (curr->key = key) {
+                if (prev == nullptr) {
+                    table[index] = curr->next;
+                } else {
+                    prev->next = curr->next;
+                }
+                delete curr;
+                return;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+};
 
 int main() {
     HashTable ht;
